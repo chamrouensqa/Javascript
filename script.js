5e8f0fcf5850d857c78091e4cf583149b044dfcd
Chamrouen <chamrouen@Chamrouens-MacBook-Air.local>
2022-08-22 20:05:14 +0700

    Taking condition if else statement

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ console.log(js);
 const name = "Chamrouen";
 console.log(name);
 console.log(`Hello my name is ${name}`);
+
+// Checking age condition
+const age = 13;
+const isOldEnought = age >= 18;
+if (isOldEnought){
+    console.log("You can start driving licience...🚗");
+} else{
+    const yearLeft = 18 - age;
+    console.log(`You are you young...You need ${yearLeft} years to go`);
+}
+
+// Check the century of my specific age
+let century;
+const birthYear = 2001;
+if(birthYear <= 2000){
+    century = 20;
+}else{
+    century = 21;
+}
+console.log(`Your birthday century is ${century}`);
